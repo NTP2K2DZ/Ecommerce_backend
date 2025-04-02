aef3ed3f52a1482ee3f5aa3d18ab778f7bdea355
ntp2k2dz <ndbestyasuo@gmail.com>
2025-04-02 17:52:32 +0700

    setup project

---
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.UserCreationRequest;
+import com.example.backend.entity.User;
+import com.example.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    User createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUser() {
+        return userService.getUser();
+    }
+    @GetMapping("/{userId}")
+    User getUserByID(@PathVariable("userId") String userId) {
+        return userService.getUser(userId);
+    }
+}
---
+++ src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,41 @@
+package com.example.backend.service;
+
+import com.example.backend.dto.UserCreationRequest;
+import com.example.backend.entity.User;
+import com.example.backend.reponsitory.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setEmail(request.getEmail());
+        user.setPhone(request.getPhone());
+        user.setDob(request.getDob());
+        user.setAddress(request.getAddress());
+        return userRepository.save(user);
+    }
+
+    public List<User> getUser(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("user not found"));
+    }
+}
